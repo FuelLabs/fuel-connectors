f37b1d4455f9ae985b95abdf37685acba98ef19b
Hélcio Franco <helciofranco@me.com>
2024-02-26 10:19:24 -0400

    ci: build @fuels/connectors with all subpackages

--- packages/connectors/tsup.config.js
+++ packages/connectors/tsup.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => ({
   ...baseConfig(options, { withReact: false }),
   entry: ['src/index.ts'],
+  noExternal: [
+    '@fuel-connectors/fuel-development-wallet',
+    '@fuel-connectors/fuel-wallet',
+    '@fuel-connectors/fuelet-wallet',
+  ],
   minify: 'terser',
   treeshake: true,
   splitting: true,
