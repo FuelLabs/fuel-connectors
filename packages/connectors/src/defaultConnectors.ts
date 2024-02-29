e40b0bc21702442246076094479cab1b2e1cc205
Hélcio Franco <helciofranco@me.com>
2024-02-29 14:01:00 -0400

    fix: adjust build type generation

--- packages/connectors/src/defaultConnectors.ts
+++ packages/connectors/src/defaultConnectors.ts
@@ -7,7 +7,10 @@ type DefaultConnectors = {
 };
 
 export function defaultConnectors({ devMode }: DefaultConnectors = {}) {
-  const connectors = [new FuelWalletConnector(), new FueletWalletConnector()];
+  const connectors: FuelWalletConnector[] = [
+    new FuelWalletConnector(),
+    new FueletWalletConnector(),
+  ];
 
   if (devMode) {
     connectors.push(new FuelWalletDevelopmentConnector());
