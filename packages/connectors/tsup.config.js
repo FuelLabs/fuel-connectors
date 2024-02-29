5aa4df69c3412a2db76df478e3142172e8688c51
Hélcio Franco <helciofranco@me.com>
2024-02-29 12:31:09 -0400

    feat: build internal packages dts before bundle everything

--- packages/connectors/tsup.config.js
+++ packages/connectors/tsup.config.js
@@ -4,13 +4,20 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => ({
   ...baseConfig(options, { withReact: false }),
   entry: ['src/index.ts'],
-  external: ['fuels'],
+  external: ['fuels', 'json-rpc-2.0'],
   noExternal: [
     '@fuel-connectors/fuel-development-wallet',
     '@fuel-connectors/fuel-wallet',
     '@fuel-connectors/fuelet-wallet',
   ],
   minify: 'terser',
+  dts: {
+    resolve: [
+      '@fuel-connectors/fuel-development-wallet',
+      '@fuel-connectors/fuel-wallet',
+      '@fuel-connectors/fuelet-wallet',
+    ],
+  },
   splitting: true,
   metafile: true,
 }));
