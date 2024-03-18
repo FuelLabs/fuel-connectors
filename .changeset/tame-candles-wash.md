4521c96803ba1b863056ce2df0a39e0eb56b002a
Hélcio Franco <helciofranco@me.com>
2024-03-18 11:22:14 -0400

    feat: upgrade @fuels/react

---
+++ .changeset/tame-candles-wash.md
@@ -0,0 +1,8 @@
+---
+"@fuel-connectors/fuel-development-wallet": minor
+"@fuel-connectors/fuelet-wallet": minor
+"@fuel-connectors/fuel-wallet": minor
+"@fuels/connectors": minor
+---
+
+feat: bump to @fuels/react 0.17.0 which moved react-query to peerDependency.
--- examples/react-app/src/main.tsx
+++ examples/react-app/src/main.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { defaultConnectors } from '@fuels/connectors';
 import { FuelProvider } from '@fuels/react';
 
 import App from './App.tsx';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FuelProvider
-      fuelConfig={{
-        connectors: defaultConnectors({ devMode: true }),
-      }}
-    >
-      <App />
-    </FuelProvider>
+    <QueryClientProvider client={queryClient}>
+      <FuelProvider
+        fuelConfig={{
+          connectors: defaultConnectors({ devMode: true }),
+        }}
+      >
+        <App />
+      </FuelProvider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
