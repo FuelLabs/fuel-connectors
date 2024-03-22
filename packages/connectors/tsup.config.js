3e336668c41d18889d65023ba7a9a421a9d4217e
guimroque <guilhermemr_2017@hotmail.com>
2024-03-22 16:20:07 -0300

    fix(packages): remove not used packages and remove peerDependencies

--- packages/connectors/tsup.config.js
+++ packages/connectors/tsup.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => ({
   ...baseConfig(options, { withReact: false }),
   entry: ['src/index.ts'],
-  external: ['fuels', 'json-rpc-2.0', 'socket.io-client', 'axios'],
+  external: ['fuels', 'json-rpc-2.0'],
   noExternal: [
     '@fuel-connectors/fuel-development-wallet',
     '@fuel-connectors/fuel-wallet',
