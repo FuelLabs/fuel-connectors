0a5eaed845863f25ed4251ea460db2e2e158f78e
Hélcio Franco <helciofranco@me.com>
2024-03-26 11:11:38 -0400

    docs: add more example using wallet manager

--- examples/nodejs/src/index.ts
+++ examples/nodejs/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+import { Mnemonic, WalletManager } from 'fuels';
+
+const main = async () => {
+  // Mnemonic
+  const WALLET_PASSWORD = 'Qwe1234567$';
+  const mnemonic = Mnemonic.generate(16);
+
+  // Vault with initial account
+  const manager = new WalletManager();
+  await manager.unlock(WALLET_PASSWORD);
+  await manager.addVault({
+    title: '@fuels/connectors',
+    type: 'mnemonic',
+    secret: mnemonic,
+  });
+
+  // Adding second account
+  await manager.addAccount({ vaultId: 0 });
+
+  // Listing accounts
+  const accounts = manager.getAccounts();
+  console.log(mnemonic);
+  console.log(accounts.map((ac) => ac.address.toString()));
+};
+
+main();
