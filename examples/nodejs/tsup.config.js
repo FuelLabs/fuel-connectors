b4ee6dc337c190968f36c584d2d817f9c5bbc02a
Hélcio Franco <helciofranco@me.com>
2024-03-26 11:07:32 -0400

    feat: add nodejs example playable project

---
+++ examples/nodejs/tsup.config.js
@@ -0,0 +1,10 @@
+import baseConfig from '@fuels/tsup-config';
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => ({
+  ...baseConfig(options, { withReact: false }),
+  entry: ['src/index.ts'],
+  minify: false,
+  splitting: true,
+  metafile: true,
+}));
