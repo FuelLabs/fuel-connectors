c60292808569c81c878fdf26b3a2423d679e0609
guimroque <guilhermemr_2017@hotmail.com>
2024-03-26 14:34:45 -0300

    fix(logo): use logo by bako.global domain

--- packages/bako-safe/src/constants.ts
+++ packages/bako-safe/src/constants.ts
@@ -3,8 +3,9 @@ export const APP_DESCRIPTION =
   'Bako Safe is a connector to safe.bako.global, a non-custodial vault for your crypto assets.';
 
 export const APP_IMAGE_DARK =
-  'https://besafe-asset.s3.amazonaws.com/BAKO_CONNECTOR_ICON.svg';
+  'https://safe.bako.global/BAKO_CONNECTOR_ICON.svg';
 export const APP_IMAGE_LIGHT =
-  'https://besafe-asset.s3.amazonaws.com/BAKO_CONNECTOR_ICON.svg';
+  'https://safe.bako.global/BAKO_CONNECTOR_ICON.svg';
+
 export const APP_BSAFE_URL = 'https://safe.bako.global';
 export const API_URL = 'https://api-safe.bako.global';
