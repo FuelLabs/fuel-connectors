1d0cb6d62ac32ba30bfeb931b8758d8c66ee8e41
Pedro Pereira <pedro.2108@hotmail.com>
2024-03-29 17:43:15 -0300

    build: dependencies added and settings implemented

--- examples/react-app/vite.config.ts
+++ examples/react-app/vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
