2e48b4620826f9568cb1e3bded2ab73969a0a35f
Pedro Pereira <pedro.2108@hotmail.com>
2024-03-29 19:47:25 -0300

    fix: add a limit height to avoid the icon changes the box size

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -40,11 +40,13 @@ export default function App() {
                 id="text"
                 className="col-span-3 px-4 py-8 sm:px-8 sm:py-8 md:px-10 md:py-12"
               >
-                <img
-                  src={currentConnector.logo}
-                  alt={currentConnector.title}
-                  className="w-24"
-                />
+                <div className="flex h-20 items-end">
+                  <img
+                    src={currentConnector.logo}
+                    alt={currentConnector.title}
+                    className="w-20"
+                  />
+                </div>
                 <h1 className="pb-1 pt-6 text-3xl font-medium">
                   {currentConnector.title}
                 </h1>
