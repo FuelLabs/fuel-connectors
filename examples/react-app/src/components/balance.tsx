d2faee0308d2fa8bd95c8fc6e5ccbee1e0a84954
Pedro Pereira <98242932+pedropereiradev@users.noreply.github.com>
2024-04-01 15:58:39 -0300

    feat: New Wallet Connector UI (#24)

---
+++ examples/react-app/src/components/balance.tsx
@@ -0,0 +1,33 @@
+import { bn } from 'fuels';
+import { useEffect } from 'react';
+import { useWallet } from '../hooks/useWallet';
+import Feature from './feature';
+
+export const DEFAULT_AMOUNT = bn.parseUnits('0.0001');
+
+const BalanceSkeleton = () => (
+  <div className="h-6 w-28 animate-pulse bg-gray-800" />
+);
+
+export default function Balance() {
+  const { refetchWallet, balance, address } = useWallet();
+
+  useEffect(() => {
+    const interval = setInterval(() => refetchWallet(), 5000);
+    return () => clearInterval(interval);
+  }, [refetchWallet]);
+
+  return (
+    <Feature title="Balance">
+      <code>{balance ? `${balance?.format()} ETH` : <BalanceSkeleton />}</code>
+      <a
+        href={`https://faucet-beta-5.fuel.network/?address=${address}`}
+        target="_blank"
+        className="btn btn-primary"
+        rel="noreferrer"
+      >
+        Get coins
+      </a>
+    </Feature>
+  );
+}
---
+++ examples/react-app/src/utils/customError.ts
@@ -0,0 +1,3 @@
+export interface CustomError extends Error {
+  message: string;
+}
