dce786fc81257bb236c3f89851ab5b2ef155745c
Pedro Pereira <pedro.2108@hotmail.com>
2024-04-02 10:40:57 -0300

    feat: evm wallet connector events standardized

--- packages/evm-connector/src/types.ts
+++ packages/evm-connector/src/types.ts
@@ -5,3 +5,12 @@ export type EVMWalletConnectorConfig = {
   fuelProvider?: Provider | string;
   ethProvider?: EIP1193Provider;
 };
+
+export enum EVMWalletConnectorEvents {
+  //accounts
+  ACCOUNTS_CHANGED = 'accountsChanged',
+
+  //connections
+  CONNECT = 'connect',
+  DISCONNECT = 'disconnect',
+}
