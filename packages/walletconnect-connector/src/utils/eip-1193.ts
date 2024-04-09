f72d82c3a6b2bc9298f47e9abe36648c8381fb2d
Pedro Pereira <pedro.2108@hotmail.com>
2024-04-09 11:32:48 -0300

    chore: remove unused files

--- packages/walletconnect-connector/src/utils/eip-1193.ts
+++
@@ -1,7 +0,0 @@
-import type EventEmitter from 'node:events';
-
-export interface EIP1193Provider extends EventEmitter {
-  // biome-ignore lint/suspicious/noExplicitAny:
-  request(args: { method: string; params?: any[] }): Promise<any>;
-  providers?: EIP1193Provider[];
-}
