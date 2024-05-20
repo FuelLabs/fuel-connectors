6d0f9f6125aa575c618c2a7e76db8c1c2714a819
Peter Smith <peter@blueoceancomputing.co.uk>
2024-05-20 06:11:14 +0100

    chore: remove type hint from predicates

--- packages/walletconnect-connector/src/generated/predicate.ts
+++ packages/walletconnect-connector/src/generated/predicate.ts
@@ -1,9 +1,8 @@
 import { arrayify } from 'fuels';
 import type { JsonAbi } from 'fuels';
-import type { PredicateConfig } from '../types';
 import { VerificationPredicateAbi__factory } from './predicates';
 
-export const predicates: Record<string, PredicateConfig> = {
+export const predicates = {
   'verification-predicate': {
     abi: VerificationPredicateAbi__factory.abi as JsonAbi,
     bytecode: arrayify(VerificationPredicateAbi__factory.bin),
