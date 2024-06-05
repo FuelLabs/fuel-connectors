9782bd872c123ec8ec6125f4645860874c083ea9
Pedro Pereira <pedro.2108@hotmail.com>
2024-06-05 16:02:18 -0300

    refactor: generate predicate resources function identetion

--- packages/solana-connector/scripts/generatePredicateResources.ts
+++ packages/solana-connector/scripts/generatePredicateResources.ts
@@ -26,7 +26,7 @@ function base64ToUint8Array(base64: string) {
   const binaryString = atob(base64);
   const bytes = new Uint8Array(binaryString.length);
   for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
+      bytes[i] = binaryString.charCodeAt(i);
   }
   return bytes;
 }
