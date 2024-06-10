e764a5c9617f35808c86888b9ddfa27dbd2c547f
guimroque <guilhermemr_2017@hotmail.com>
2024-06-10 20:16:47 -0300

    fix: remove evm export

--- packages/connectors/src/index.ts
+++ packages/connectors/src/index.ts
@@ -3,5 +3,4 @@ export * from '@fuel-connectors/fuel-development-wallet';
 export * from '@fuel-connectors/fuel-wallet';
 export * from '@fuel-connectors/fuelet-wallet';
 export * from '@fuel-connectors/bako-safe';
-export * from '@fuel-connectors/evm-connector';
 export * from '@fuel-connectors/burner-wallet-connector';
