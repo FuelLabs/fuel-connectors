5f837eef25a09e10877e849dfa502b848693c31b
guimroque <guilhermemr_2017@hotmail.com>
2024-06-10 20:22:36 -0300

    chore: remove unused import

--- packages/connectors/src/defaultConnectors.ts
+++ packages/connectors/src/defaultConnectors.ts
@@ -1,6 +1,5 @@
 import { BakoSafeConnector } from '@fuel-connectors/bako-safe';
 import { BurnerWalletConnector } from '@fuel-connectors/burner-wallet-connector';
-import { EVMWalletConnector } from '@fuel-connectors/evm-connector';
 import { FuelWalletDevelopmentConnector } from '@fuel-connectors/fuel-development-wallet';
 import { FuelWalletConnector } from '@fuel-connectors/fuel-wallet';
 import { FueletWalletConnector } from '@fuel-connectors/fuelet-wallet';
