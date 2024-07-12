481b5fea5363f38daf0c054712395cfc7e2227e0
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-07-12 15:05:03 -0300

    fix: burner wallet storage incompatibility with Node.js env (#156)

--- packages/connectors/src/defaultConnectors.ts
+++ packages/connectors/src/defaultConnectors.ts
@@ -5,20 +5,23 @@ import { FuelWalletConnector } from '@fuel-connectors/fuel-wallet';
 import { FueletWalletConnector } from '@fuel-connectors/fuelet-wallet';
 import { WalletConnectConnector } from '@fuel-connectors/walletconnect-connector';
 import type { FuelConnector } from 'fuels';
+import type { BurnerWalletConfig } from '../../burner-wallet-connector/src/types';
 
 type DefaultConnectors = {
   devMode?: boolean;
+  burnerWalletConfig?: BurnerWalletConfig;
 };
 
 export function defaultConnectors({
   devMode,
+  burnerWalletConfig,
 }: DefaultConnectors = {}): Array<FuelConnector> {
   const connectors = [
     new FuelWalletConnector(),
     new BakoSafeConnector(),
     new FueletWalletConnector(),
     new WalletConnectConnector(),
-    new BurnerWalletConnector(),
+    new BurnerWalletConnector(burnerWalletConfig),
   ];
 
   if (devMode) {
