fd38955b631de095c5c096606d030854cd98d24e
Leonardo C. Martins <leocourbassier@gmail.com>
2024-07-13 01:17:15 -0300

    fix: update vite.config.ts

--- packages/evm-connector/vite.config.ts
+++ packages/evm-connector/vite.config.ts
@@ -5,14 +5,12 @@ import dts from 'vite-plugin-dts';
 
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     dts({
       include: [resolve(__dirname, 'src/')],
     }),
-    tsconfigPaths(),
   ],
   define: {
     __dirname: JSON.stringify(path.dirname(__filename)),
