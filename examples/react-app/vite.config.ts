36bad68c0739a29a652e14cae02eb25726e20f66
LuizAsFight <felipebolsonigomes@gmail.com>
2024-08-03 19:25:32 -0300

    chore: fix build

--- examples/react-app/vite.config.ts
+++ examples/react-app/vite.config.ts
@@ -2,9 +2,22 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    nodePolyfills({
+      exclude: ['fs'],
+      globals: {
+        // Don't polyfill these globals
+        process: false,
+        Buffer: false,
+      },
+      protocolImports: false,
+    }),
+    react(),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
