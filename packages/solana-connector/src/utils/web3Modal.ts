a8691b4988f64087ce917222cc2f8ebe442215c6
Hélcio Franco <helciofranco@me.com>
2024-08-05 17:25:15 -0400

    feat: add phantom wallet in the solana connectors

--- packages/solana-connector/src/utils/web3Modal.ts
+++ packages/solana-connector/src/utils/web3Modal.ts
@@ -25,5 +25,10 @@ export function createSolanaWeb3ModalInstance({
     chains: DEFAULT_CHAINS,
     enableAnalytics: false,
     projectId: projectId ?? DEFAULT_PROJECT_ID,
+    featuredWalletIds: [
+      'a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393', // Phantom
+      '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+      '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKX Wallet
+    ],
   });
 }
