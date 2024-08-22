ac8a54896e16144ba0d5b763cd9dff7459166a3b
Leonardo C. Martins <leocourbassier@gmail.com>
2024-08-22 13:19:03 -0300

    doc: add hooks reference to docs

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -63,6 +63,17 @@ export default defineConfig({
               },
             ],
           },
+          {
+            text: 'React Hooks',
+            link: '/guide/react-hooks/',
+            collapsed: true,
+            items: [
+              {
+                text: 'Hooks Reference',
+                link: '/guide/react-hooks/hooks-reference',
+              },
+            ],
+          },
           apiLinks,
         ],
       },
