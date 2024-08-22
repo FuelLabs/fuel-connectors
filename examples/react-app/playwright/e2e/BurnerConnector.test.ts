cf286fa3a8309aa37dc0f329d97ea8013bd0ce9b
Nelito Junior <ngvjqp@gmail.com>
2024-08-22 13:54:34 -0300

    chore: Refactor BurnerConnector e2e tests

--- examples/react-app/playwright/e2e/BurnerConnector.test.ts
+++ examples/react-app/playwright/e2e/BurnerConnector.test.ts
@@ -1,15 +1,45 @@
-import { downloadFuel } from '@fuels/playwright-utils';
 import { test } from '@fuels/playwright-utils';
 import { expect } from '@playwright/test';
 import { connectBurner } from './utils';
 
-const fuelPathToExtension = await downloadFuel('0.21.0');
-test.use({ pathToExtension: fuelPathToExtension });
-
 test.describe('BurnerWalletConnector', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await page.bringToFront();
+  });
+
   test('should connect and show fuel address', async ({ page }) => {
     await connectBurner(page);
 
     expect(await page.waitForSelector('text=Your Fuel Address')).toBeTruthy();
+  });
+
+  test('should connect, disconnect, and reconnect', async ({ page }) => {
+    await connectBurner(page);
+
+    await page.click('text=Disconnect');
+    await page.waitForSelector('text=Connect Wallet');
+
+    await connectBurner(page);
+    expect(await page.waitForSelector('text=Your Fuel Address')).toBeTruthy();
+  });
+
+  test('should connect, refresh and stay connected', async ({ page }) => {
+    await connectBurner(page);
+
+    await page.reload();
+    await page.waitForSelector('text=Your Fuel Address');
+  });
+
+  test('should connect, disconnect, refresh and stay disconnected', async ({
+    page,
+  }) => {
+    await connectBurner(page);
+
+    await page.click('text=Disconnect');
+    await page.waitForSelector('text=Connect Wallet');
+
+    await page.reload();
+    await page.waitForSelector('text=Connect Wallet');
   });
 });
