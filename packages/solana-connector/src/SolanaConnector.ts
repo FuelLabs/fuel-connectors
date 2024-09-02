b5335f7a88d89d6349d197d9c1e95c0518a16497
Hélcio Franco <github@helciofranco.com>
2024-09-02 16:34:29 -0400

    feat: remove automatic balance transfer between predicates

--- packages/solana-connector/src/SolanaConnector.ts
+++ packages/solana-connector/src/SolanaConnector.ts
@@ -224,7 +224,7 @@ export class SolanaConnector extends PredicateConnector {
     address: string,
     transaction: TransactionRequestLike,
   ): Promise<string> {
-    const { predicate, transactionId, transactionRequest, afterTransaction } =
+    const { predicate, transactionId, transactionRequest } =
       await this.prepareTransaction(address, transaction);
 
     const predicateSignatureIndex = getMockedSignatureIndex(
@@ -246,8 +246,6 @@ export class SolanaConnector extends PredicateConnector {
     await predicate.provider.estimatePredicates(transactionRequest);
 
     const response = await predicate.sendTransaction(transactionRequest);
-
-    afterTransaction?.(response.id);
 
     return response.id;
   }
--- packages/walletconnect-connector/src/WalletConnectConnector.ts
+++ packages/walletconnect-connector/src/WalletConnectConnector.ts
@@ -248,13 +248,8 @@ export class WalletConnectConnector extends PredicateConnector {
     transaction: TransactionRequestLike,
   ): Promise<string> {
     const { ethProvider, fuelProvider } = await this.getProviders();
-    const {
-      request,
-      transactionId,
-      account,
-      transactionRequest,
-      afterTransaction,
-    } = await this.prepareTransaction(address, transaction);
+    const { request, transactionId, account, transactionRequest } =
+      await this.prepareTransaction(address, transaction);
 
     const signature = (await ethProvider?.request({
       method: 'personal_sign',
@@ -277,8 +272,6 @@ export class WalletConnectConnector extends PredicateConnector {
         transactionWithPredicateEstimated.toTransactionBytes(),
       ),
     });
-
-    afterTransaction?.(response.submit.id);
 
     return response.submit.id;
   }
