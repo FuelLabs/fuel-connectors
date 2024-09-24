4baa0ba6815655dd61ae00d867084ad99d404142
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-09-24 10:50:44 -0300

    feat: fuel chain provider

---
+++ packages/react/src/providers/FuelChainProvider.tsx
@@ -0,0 +1,24 @@
+import type { Fuel, Network } from 'fuels';
+import { createContext, useContext } from 'react';
+
+const context = createContext<Network['chainId'] | null | undefined>(null);
+
+/**
+ * A hook that returns the target chain id for the active provider.
+ *
+ * @examples
+ * ```ts
+ * const { chainId } = useFuelChain();
+ * console.log(chainId);
+ * ```
+ */
+export function useFuelChain() {
+  const contextData = useContext(context);
+
+  if (contextData === null) {
+    throw new Error('useFuelChain must be used within a FuelChainProvider');
+  }
+  return { chainId: contextData };
+}
+
+export const FuelChainProvider = context.Provider;
