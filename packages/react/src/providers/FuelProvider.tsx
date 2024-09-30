f1a0a237a204aee555e5a2fb166b8f143377cc21
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-09-30 17:59:35 -0300

    fix: FuelProvider leaking render events (#316)

--- packages/react/src/providers/FuelProvider.tsx
+++ packages/react/src/providers/FuelProvider.tsx
@@ -2,6 +2,7 @@ import type { FuelConfig } from 'fuels';
 
 import { Connect } from '../ui/Connect';
 
+import { useMemo } from 'react';
 import type { NetworkConfig, UIConfig } from '../types';
 import { BridgeDialog } from '../ui/Connect/components/Bridge/BridgeDialog';
 import { NetworkDialog } from '../ui/Connect/components/Network/NetworkDialog';
@@ -29,11 +30,15 @@ export function FuelProvider({
 }: FuelProviderProps) {
   const theme = _theme || 'light';
   const networksConfig = useNetworkConfigs(networks);
-  const uiConfig = Object.assign(
-    {
-      suggestBridge: true,
-    },
-    _uiConfig ?? {},
+  const uiConfig = useMemo(
+    () =>
+      Object.assign(
+        {
+          suggestBridge: true,
+        },
+        _uiConfig ?? {},
+      ),
+    [_uiConfig],
   );
 
   if (ui) {
