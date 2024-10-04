c9ef1bb9d8fa4aa801daaf06a94a781d40fda335
luisburigo <guga.burigo29@outlook.com>
2024-10-04 12:54:01 -0300

    fix(bako-safe): remove iframe on safari and open in new tab

--- packages/bako-safe/src/DAPPWindow.ts
+++ packages/bako-safe/src/DAPPWindow.ts
@@ -48,7 +48,7 @@ export class DAppWindow {
     if (this.isSafariBrowser) {
       // && !isConnection) {
       // to confirm tx, we need a new popup
-      this.makeFrame(method, true);
+      this.makePage(method);
     }
 
     return;
@@ -74,37 +74,12 @@ export class DAppWindow {
     this.isOpen = true;
   }
 
-  makeFrame(method: string, isSafari = false) {
-    const w = this.small;
-    //bako frame
-    const frame = document.createElement('iframe');
-    frame.id = `${this.config.sessionId}-iframe`;
-    frame.src = `${this.config.appUrl}${method}${this.queryString}${
-      isSafari ? '&byConnector=true' : ''
-    }`;
-    frame.style.position = 'fixed';
-    frame.style.zIndex = '99999999';
-    frame.style.top = `${w.top}`;
-    frame.style.left = `${w.left}`;
-    frame.style.width = w.width;
-    frame.style.height = w.height;
-    frame.style.borderRadius = '16px';
-
-    //backdrop
-    const backdrop = document.createElement('div');
-    backdrop.id = `${this.config.sessionId}-backdrop`;
-    backdrop.style.position = 'fixed';
-    backdrop.style.top = '0';
-    backdrop.style.left = '0';
-    backdrop.style.width = '100%';
-    backdrop.style.height = '100%';
-    backdrop.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    backdrop.style.zIndex = '99999998';
-    backdrop.addEventListener('click', () => this.close()); // if user click on backdrop, close the frame
-
-    document.body.appendChild(backdrop);
-    document.body.appendChild(frame);
+  makePage(method: string) {
+    const link = `${this.config.appUrl}${method}${this.queryString}`;
+    const popup = WINDOW.open(link, '_blank');
+    if (popup) this.opned = popup;
     this.isOpen = true;
+    return popup;
   }
 
   makePopup(method: string) {
@@ -122,37 +97,5 @@ export class DAppWindow {
   private get queryString() {
     const { sessionId } = this.config;
     return `?sessionId=${sessionId}&origin=${WINDOW.location.origin}&name=${WINDOW.document.title}&request_id=${this.request_id}`;
-  }
-
-  private get small() {
-    // todo: update this to calculate by screen size changes
-    const breakponint = {
-      md: {
-        top: 0,
-        left: 0,
-        limit: 650,
-        width: '100%',
-        height: '100%',
-      }, // 100%
-      lg: {
-        top: `${(WINDOW.innerHeight - WINDOW.innerHeight * 0.7) / 2}px`,
-        left: `${(WINDOW.innerWidth - WINDOW.innerWidth * 0.5) / 2}px`,
-        limit: 1024,
-        width: '50%',
-        height: '70%',
-      }, // 75%
-      xl: {
-        top: `${(WINDOW.innerHeight - 650) / 2}px`,
-        left: `${(WINDOW.innerWidth - 500) / 2}px`,
-        limit: 1440,
-        height: '650px',
-        width: '500px',
-      }, // 400px
-    };
-    return WINDOW.innerWidth < breakponint.md.limit
-      ? breakponint.md
-      : WINDOW.innerWidth < breakponint.lg.limit
-        ? breakponint.lg
-        : breakponint.xl;
   }
 }
