e77fb5a20a55c4f6a4142b78ddfe268d46384f0b
Hélcio Franco <github@helciofranco.com>
2024-10-06 00:26:55 -0300

    fix: reset connector list query when we get a connectors event

---
+++ .changeset/chatty-wasps-swim.md
@@ -0,0 +1,5 @@
+---
+"@fuels/react": patch
+---
+
+Fix the display of the `installed` status when a connector is `installed` while the page is open.
--- packages/react/src/ui/Connect/components/Connectors/Connectors.tsx
+++ packages/react/src/ui/Connect/components/Connectors/Connectors.tsx
@@ -1,53 +1,16 @@
-import { useMemo, useState } from 'react';
-import {
-  type FuelUIContextType,
-  useConnectUI,
-} from '../../../../providers/FuelUIProvider';
-import { ConnectorIcon } from '../Core/ConnectorIcon';
-import { ConnectorBadge } from './ConnectorBadge';
+import { useMemo } from 'react';
+import { useConnectUI } from '../../../../providers/FuelUIProvider';
 
 import type { FuelConnector } from 'fuels';
 import { NATIVE_CONNECTORS } from '../../../../config';
+import { Connector } from './Connector';
 import { ConnectorsLoader } from './ConnectorsLoader';
-import {
-  ConnectorItem,
-  ConnectorList,
-  ConnectorName,
-  GroupLastTitle,
-  GroupTitle,
-} from './styles';
+import { ConnectorList, GroupLastTitle, GroupTitle } from './styles';
 
-const renderConnector = (
-  connect: FuelUIContextType['dialog']['connect'],
-  theme: FuelUIContextType['theme'],
-  connector: FuelConnector,
-  index: number,
-) => (
-  <ConnectorItem
-    tabIndex={index + 1}
-    key={connector.name}
-    aria-label={`Connect to ${connector.name}`}
-    data-installed={connector.installed}
-    data-connected={connector.connected}
-    onClick={(e) => {
-      e.preventDefault();
-      connect(connector);
-    }}
-  >
-    <ConnectorIcon
-      connectorMetadata={connector.metadata}
-      connectorName={connector.name}
-      size={32}
-      theme={theme}
-    />
-    <ConnectorName>{connector.name}</ConnectorName>
-    <ConnectorBadge
-      name={connector.name}
-      connected={connector.connected}
-      installed={connector.installed}
-    />
-  </ConnectorItem>
-);
+interface GroupedConnectors {
+  native: FuelConnector[];
+  external: FuelConnector[];
+}
 
 export function Connectors() {
   const {
@@ -58,40 +21,58 @@ export function Connectors() {
     dialog: { connect },
   } = useConnectUI();
 
-  const [renderConnectorItem] = useState(() =>
-    renderConnector.bind(null, connect, theme),
-  );
+  const { native, external } = useMemo<GroupedConnectors>(() => {
+    const external = connectors.filter((conn) => {
+      return !NATIVE_CONNECTORS.includes(conn.name);
+    });
+    const native = connectors.filter((conn) => {
+      return NATIVE_CONNECTORS.includes(conn.name);
+    });
 
-  const { native, external } = useMemo(
-    () =>
-      connectors.reduce(
-        (acc, curr) => {
-          const array = NATIVE_CONNECTORS.includes(curr.name)
-            ? acc.native
-            : acc.external;
-          array.push(curr);
+    return {
+      native,
+      external,
+    };
+  }, [connectors]);
 
-          return acc;
-        },
-        {
-          native: [] as Array<FuelConnector>,
-          external: [] as Array<FuelConnector>,
-        },
-      ),
-    [connectors],
-  );
+  const shouldTitleGroups = !!native.length && !!external.length;
 
-  const shouldTitleGroups = !isLoading && !!native.length && !!external.length;
+  console.log(connectors.map((c) => `${c.name} ${c.installed} ${c.connected}`));
+
+  if (isLoading) {
+    return (
+      <ConnectorList>
+        <ConnectorsLoader items={fuelConfig.connectors?.length || 2} />
+      </ConnectorList>
+    );
+  }
 
   return (
     <ConnectorList>
       {shouldTitleGroups && <GroupTitle>Fuel Native Wallets</GroupTitle>}
-      {!isLoading && native.map(renderConnectorItem)}
+      {native.map((connector, index) => {
+        return (
+          <Connector
+            key={connector.name}
+            connect={connect}
+            theme={theme}
+            connector={connector}
+            index={index}
+          />
+        );
+      })}
       {shouldTitleGroups && <GroupLastTitle>Non-Native Wallets</GroupLastTitle>}
-      {!isLoading && external.map(renderConnectorItem)}
-      {isLoading && (
-        <ConnectorsLoader items={fuelConfig.connectors?.length || 2} />
-      )}
+      {external.map((connector, index) => {
+        return (
+          <Connector
+            key={connector.name}
+            connect={connect}
+            theme={theme}
+            connector={connector}
+            index={index}
+          />
+        );
+      })}
     </ConnectorList>
   );
 }
