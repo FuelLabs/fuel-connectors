45bfeb4a5675a9c800da169a251ce7a45465d1a1
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-10-07 17:12:23 -0300

    feat: create local contract id

--- examples/react-app/src/config.ts
+++ examples/react-app/src/config.ts
@@ -1,6 +1,7 @@
 import { type CHAIN_IDS, bn } from 'fuels';
+import { counter as COUNTER_CONTRACT_ID_LOCAL } from './types/contract-ids-local.json';
 import { counter as COUNTER_CONTRACT_ID_MAINNET } from './types/contract-ids-mainnet.json';
-import { counter as COUNTER_CONTRACT_ID_TESTNET } from './types/contract-ids.json';
+import { counter as COUNTER_CONTRACT_ID_TESTNET } from './types/contract-ids-testnet.json';
 
 export const CHAIN_ID_NAME = import.meta.env
   .VITE_CHAIN_ID_NAME as keyof typeof CHAIN_IDS.fuel;
@@ -9,7 +10,15 @@ export const DEFAULT_AMOUNT = bn.parseUnits(
   CHAIN_ID_NAME === 'mainnet' ? '0.000000001' : '0.0001',
 );
 
-export const COUNTER_CONTRACT_ID =
-  CHAIN_ID_NAME === 'mainnet'
-    ? COUNTER_CONTRACT_ID_MAINNET
-    : COUNTER_CONTRACT_ID_TESTNET;
+function getContractId() {
+  switch (CHAIN_ID_NAME) {
+    case 'mainnet':
+      return COUNTER_CONTRACT_ID_MAINNET;
+    case 'testnet':
+      return COUNTER_CONTRACT_ID_TESTNET;
+    default:
+      return COUNTER_CONTRACT_ID_LOCAL;
+  }
+}
+
+export const COUNTER_CONTRACT_ID = getContractId();
