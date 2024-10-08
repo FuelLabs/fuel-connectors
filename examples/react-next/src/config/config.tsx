7b3870863bde96a948299685efecb3f7bf0d088a
LuizAsFight <felipebolsonigomes@gmail.com>
2024-10-08 04:45:13 -0300

    feat: improve react-next setup

---
+++ examples/react-next/src/config/config.tsx
@@ -0,0 +1,38 @@
+import { generateETHConnectors } from '@/utils/connectors';
+import {
+  http,
+  cookieStorage,
+  createConfig,
+  createStorage,
+  fallback,
+} from 'wagmi';
+import { type Chain, sepolia } from 'wagmi/chains';
+
+export const APP = {
+  name: 'Fuel Bridge',
+  description: 'Bridge assets between Fuel and Other Chains',
+  url: 'https://fuels-portal.vercel.app',
+  icons: ['https://fuels-portal.vercel.app/fuel-logo.svg'],
+};
+export const CHAINS_TO_CONNECT = [sepolia] as [Chain, ...Chain[]];
+
+export const TRANSPORTS = {
+  [CHAINS_TO_CONNECT[0].id]: fallback(
+    [
+      http(
+        `https://eth-${CHAINS_TO_CONNECT[0].name}.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_WC_PROJECT_ID}`,
+      ),
+      http(),
+    ],
+    { rank: false },
+  ),
+};
+export const DEFAULT_WAGMI_CONFIG = createConfig({
+  chains: CHAINS_TO_CONNECT,
+  connectors: generateETHConnectors(APP.name),
+  transports: TRANSPORTS,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+});
