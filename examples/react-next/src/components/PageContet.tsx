741c69a13105fdeff280c31e5330cfc48c8484f3
luizstacio <luizstacio@gmail.com>
2024-10-09 18:23:04 -0300

    feat: modify examples to follow SSR

---
+++ examples/react-next/src/components/PageContet.tsx
@@ -0,0 +1,50 @@
+'use client';
+import {
+  useAccounts,
+  useConnectUI,
+  useDisconnect,
+  useIsConnected,
+  useWallet,
+} from '@fuels/react';
+
+export default function PageContent() {
+  const { connect, error, isError, isConnecting } = useConnectUI();
+  const { disconnect } = useDisconnect();
+  const { isConnected } = useIsConnected();
+  const { accounts } = useAccounts();
+  const { wallet } = useWallet();
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          console.log('connect');
+          connect();
+        }}
+      >
+        {isConnecting ? 'Connecting' : 'Connect'}
+      </button>
+      {isConnected && (
+        <button type="button" onClick={() => disconnect()}>
+          Disconnect
+        </button>
+      )}
+
+      {isError && <p className="Error">{error?.message}</p>}
+
+      {wallet && <div>Wallet: {wallet.address.toString()}</div>}
+
+      {isConnected && (
+        <div>
+          <h3>Connected accounts</h3>
+          {accounts?.map((account) => (
+            <div key={account}>
+              <b>Account:</b> {account}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
