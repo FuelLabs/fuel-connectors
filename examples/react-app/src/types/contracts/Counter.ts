01f72ea975373849988f063e905fff53578d73bc
Anderson Arboleya <anderson@arboleya.me>
2024-10-12 23:45:30 -0300

    Upgrading `fuels` to `0.96.0`

--- examples/react-app/src/types/contracts/Counter.ts
+++ examples/react-app/src/types/contracts/Counter.ts
@@ -5,9 +5,9 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
 /*
-  Fuels version: 0.94.9
-  Forc version: 0.65.2
-  Fuel-Core version: 0.37.1
+  Fuels version: 0.96.0
+  Forc version: 0.65.1
+  Fuel-Core version: 0.37.0
 */
 
 import { Contract, Interface } from 'fuels';
