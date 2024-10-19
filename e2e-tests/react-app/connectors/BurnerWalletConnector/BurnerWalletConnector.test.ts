70b71395bf713a05776cf43c5bfb4bb5eddefbe1
Nelito Junior <ngvjqp@gmail.com>
2024-10-19 20:06:26 -0300

    test: Update BurnerWalletConnector e2e tests

--- e2e-tests/react-app/connectors/BurnerWalletConnector/BurnerWalletConnector.test.ts
+++ e2e-tests/react-app/connectors/BurnerWalletConnector/BurnerWalletConnector.test.ts
@@ -8,7 +8,7 @@ const connectBurner = async (page: Page, walletName = 'Burner Wallet') => {
   await getByAriaLabel(page, `Connect to ${walletName}`, true).click();
 };
 
-test.describe('BurnerWalletConnector', () => {
+test.describe('BurnerWalletConnector', async () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     await page.bringToFront();
@@ -17,43 +17,29 @@ test.describe('BurnerWalletConnector', () => {
   test('should connect and show fuel address', async ({ page }) => {
     await connectBurner(page);
 
-    expect(await page.waitForSelector('text=Your Fuel Address')).toBeTruthy();
+    expect(await page.waitForSelector('text=/Your Fuel Address/')).toBeTruthy();
 
-    await page.click('text=Disconnect');
-    await page.waitForSelector('text=Connect Wallet');
+    await test.step('should connect, disconnect, and reconnect', async () => {
+      await page.click('text=Disconnect');
+      await page.waitForSelector('text=/Connect Wallet/');
 
-    await connectBurner(page);
-    expect(await page.waitForSelector('text=Your Fuel Address')).toBeTruthy();
+      await connectBurner(page);
+      expect(
+        await page.waitForSelector('text=/Your Fuel Address/'),
+      ).toBeTruthy();
+    });
 
-    await page.reload();
-    expect(await page.waitForSelector('text=Your Fuel Address')).toBeTruthy();
+    await test.step('should connect, refresh and stay connected', async () => {
+      await page.reload();
+      await page.waitForSelector('text=/Your Fuel Address/');
+    });
 
-    await page.click('text=Disconnect');
-    expect(await page.waitForSelector('text=Connect Wallet')).toBeTruthy();
+    await test.step('should connect, disconnect, refresh and stay disconnected', async () => {
+      await page.click('text=Disconnect');
+      await page.waitForSelector('text=/Connect Wallet/');
 
-    await page.reload();
-    expect(await page.waitForSelector('text=Connect Wallet')).toBeTruthy();
+      await page.reload();
+      await page.waitForSelector('text=/Connect Wallet/');
+    });
   });
-
-  // test('should increment counter', async ({ page }) => {
-  //   await connectBurner(page);
-
-  //   await page.click('text=Increment');
-
-  //   expect(await page.waitForSelector('text=Success')).toBeTruthy();
-  //   expect(
-  //     await page.waitForSelector('text=Counter Incremented!'),
-  //   ).toBeTruthy();
-  // });
-
-  // test('should execute transfer', async ({ page }) => {
-  //   await connectBurner(page);
-
-  //   await page.click('text=Transfer 0.0001 ETH');
-
-  //   expect(await page.waitForSelector('text=Success')).toBeTruthy();
-  //   expect(
-  //     await page.waitForSelector('text=Transferred successfully!'),
-  //   ).toBeTruthy();
-  // });
 });
