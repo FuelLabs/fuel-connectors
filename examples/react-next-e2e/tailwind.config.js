e7e7bfaa9e91d71c49210747ae9998733b61f705
Nelito Junior <ngvjqp@gmail.com>
2024-10-28 18:44:19 -0300

    fix: next e2e tailwind and assets

--- examples/react-next-e2e/tailwind.config.js
+++ examples/react-next-e2e/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    '../react-app/src/**/*.{js,jsx,ts,tsx}',
+  ],
   darkMode: ['selector', '[data-theme="dark"]'],
   theme: {
     fontFamily: {
