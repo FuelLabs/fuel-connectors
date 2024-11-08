e99df6a904d7953d86022ae445e8f184aca41313
Nelito Junior <ngvjqp@gmail.com>
2024-11-08 12:37:58 +0700

    chore: clean merge

--- e2e-tests/runner/examples/connectors/BurnerWalletConnector/BurnerWalletConnector.test.ts
+++ e2e-tests/runner/examples/connectors/BurnerWalletConnector/BurnerWalletConnector.test.ts
@@ -1,19 +1,17 @@
 import { getButtonByText, getByAriaLabel, test } from '@fuels/playwright-utils';
 import { type Page, expect } from '@playwright/test';
+import { sessionTests, skipBridgeFunds } from '../../../common/common';
+import type { ConnectFunction } from '../../../common/types';
 
-const connectBurner = async (page: Page, walletName = 'Burner Wallet') => {
+const connect: ConnectFunction = async (page: Page) => {
   await page.bringToFront();
   const connectButton = getButtonByText(page, 'Connect');
   await connectButton.click();
-  await getByAriaLabel(page, `Connect to ${walletName}`, true).click();
+  await getByAriaLabel(page, 'Connect to Burner Wallet', true).click();
 
   await skipBridgeFunds(page);
-};
 
-const skipBridgeFunds = async (page: Page) => {
-  if (await page.isVisible('text=Bridge Funds')) {
-    await page.click('text=Continue to application');
-  }
+  expect(await page.waitForSelector('text=/Your Fuel Address/')).toBeTruthy();
 };
 
 test.describe('BurnerWalletConnector', async () => {
@@ -23,33 +21,6 @@ test.describe('BurnerWalletConnector', async () => {
   });
 
   test('BurnerWallet Tests', async ({ page }) => {
-    await connectBurner(page);
-
-    expect(await page.waitForSelector('text=/Your Fuel Address/')).toBeTruthy();
-
-    await test.step('should connect, disconnect, and reconnect', async () => {
-      await page.click('text=Disconnect');
-      await page.waitForSelector('text=/Connect Wallet/');
-
-      await connectBurner(page);
-      expect(
-        await page.waitForSelector('text=/Your Fuel Address/'),
-      ).toBeTruthy();
-    });
-
-    await test.step('should connect, refresh and stay connected', async () => {
-      await page.reload();
-      await skipBridgeFunds(page);
-      await page.waitForSelector('text=/Your Fuel Address/');
-    });
-
-    await test.step('should connect, disconnect, refresh and stay disconnected', async () => {
-      await skipBridgeFunds(page);
-      await page.click('text=Disconnect');
-      await page.waitForSelector('text=/Connect Wallet/');
-
-      await page.reload();
-      await page.waitForSelector('text=/Connect Wallet/');
-    });
+    await sessionTests(page, { connect, approveTransfer: async () => {} });
   });
 });
