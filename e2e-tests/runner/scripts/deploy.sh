b641ea8cbab0ab11a8fac692b217c73a7df6e595
Nelito Junior <ngvjqp@gmail.com>
2024-11-12 14:40:29 -0300

    fix(deploy): remove unused output path for react-next and update echo message

--- e2e-tests/runner/scripts/deploy.sh
+++ e2e-tests/runner/scripts/deploy.sh
@@ -12,13 +12,11 @@ pnpm fuels deploy
 
 # Define paths for the input and both output files
 CONTRACT_IDS_PATH="$SCRIPT_DIR/../src/contract-ids.json"
-OUTPUT_PATH_NEXT="$SCRIPT_DIR/../../../examples/react-next/src/types/contract-ids-local.json"
 OUTPUT_PATH_APP="$SCRIPT_DIR/../../../examples/react-app/src/types/contract-ids-local.json"
 
 # Extract the LocalIncrement contract ID from contract-ids.json
 CONTRACT_ID=$(jq -r '.LocalIncrement' "$CONTRACT_IDS_PATH")
 
-jq -n --arg counter "$CONTRACT_ID" '{ "counter": $counter }' > "$OUTPUT_PATH_NEXT"
 jq -n --arg counter "$CONTRACT_ID" '{ "counter": $counter }' > "$OUTPUT_PATH_APP"
 
-echo "Saved contract ID as 'counter' in both contract-ids-local.json files"
+echo "Saved contract ID as 'counter' in  contract-ids-local.json"
