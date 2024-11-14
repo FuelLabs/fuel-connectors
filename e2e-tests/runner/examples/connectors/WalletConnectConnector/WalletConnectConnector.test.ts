5a4e77a25fe5d3ff9bac849048a8120334c8c987
Nelito Junior <ngvjqp@gmail.com>
2024-11-14 12:07:28 -0300

    feat(e2e-tests): add tests for FuelWalletConnector with MetaMask integration

---
+++ e2e-tests/runner/examples/connectors/WalletConnectConnector/WalletConnectConnector.test.ts
@@ -0,0 +1,51 @@
+import {
+  downloadFuel,
+  getButtonByText,
+  getByAriaLabel,
+} from '@fuels/playwright-utils';
+import { testWithSynpress } from '@synthetixio/synpress';
+import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright';
+import { sessionTests, skipBridgeFunds } from '../../../common/common';
+import type { ConnectorFunctions } from '../../../common/types';
+import basicSetup from '../../../wallet-setup/basic.setup';
+
+// Set up the test environment with Synpress and MetaMask fixtures, using the basic setup configuration
+const test = testWithSynpress(metaMaskFixtures(basicSetup));
+
+test.describe('FuelWalletConnector', () => {
+  let metamask: MetaMask;
+
+  test.beforeEach(async ({ context, extensionId, metamaskPage, page }) => {
+    metamask = new MetaMask(
+      context,
+      metamaskPage,
+      basicSetup.walletPassword,
+      extensionId,
+    );
+    await page.goto('/');
+  });
+
+  const connect: ConnectorFunctions['connect'] = async (page) => {
+    await page.goto('/');
+    const connectButton = getButtonByText(page, 'Connect Wallet', true);
+    await connectButton.click();
+    await getByAriaLabel(page, 'Connect to Ethereum Wallets', true).click();
+
+    await page.getByText('Proceed anyway').click();
+    await getButtonByText(page, 'MetaMask', true).click();
+
+    await metamask.connectToDapp();
+    // wait 3 seconds for the connection to be established
+    await page.waitForTimeout(3000);
+
+    await metamask.confirmSignature();
+  };
+
+  const approveTransfer: ConnectorFunctions['approveTransfer'] = async () => {
+    await metamask.confirmTransaction();
+  };
+
+  test('Fuel tests', async ({ page }) => {
+    await sessionTests(page, { connect, approveTransfer });
+  });
+});
