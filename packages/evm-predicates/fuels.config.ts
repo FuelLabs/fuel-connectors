a869576219010309ed13e0ad6fe99b884e6142a7
Nelito Junior <ngvjqp@gmail.com>
2024-11-15 16:04:37 -0300

    fix(config): update provider URL to local server and set genesis private key

--- packages/evm-predicates/fuels.config.ts
+++ packages/evm-predicates/fuels.config.ts
@@ -1,10 +1,11 @@
 import { createConfig } from 'fuels';
 
 export default createConfig({
-  providerUrl: 'https://testnet.fuel.network/v1/graphql',
+  providerUrl: 'http://localhost:4000/v1/graphql',
   predicates: ['./predicate'],
   output: './src/generated/tmp',
   forcBuildFlags: ['--release'],
   // needs the private key to deploy the predicate
-  privateKey: '0x',
+  privateKey:
+    '0x94ffcc53b892684acefaebc8a3d4a595e528a8cf664eeb3ef36f1020b0809d0d', // genesis private key
 });
