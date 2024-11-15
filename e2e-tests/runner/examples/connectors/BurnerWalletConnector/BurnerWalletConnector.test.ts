1f06692676d2917f126c803faa05ff9ecb7cc3b2
Nelito Junior <ngvjqp@gmail.com>
2024-11-15 20:59:54 -0300

    test(BurnerWalletConnector): add wallet funding and transfer tests

--- e2e-tests/runner/examples/connectors/BurnerWalletConnector/BurnerWalletConnector.test.ts
+++ e2e-tests/runner/examples/connectors/BurnerWalletConnector/BurnerWalletConnector.test.ts
@@ -1,7 +1,12 @@
 import { getButtonByText, getByAriaLabel, test } from '@fuels/playwright-utils';
 import { type Page, expect } from '@playwright/test';
-import { sessionTests, skipBridgeFunds } from '../../../common/common';
+import {
+  sessionTests,
+  skipBridgeFunds,
+  transferTests,
+} from '../../../common/common';
 import type { ConnectFunction } from '../../../common/types';
+import { fundWallet } from '../setup';
 
 const connect: ConnectFunction = async (page: Page) => {
   await page.bringToFront();
@@ -22,5 +27,28 @@ test.describe('BurnerWalletConnector', async () => {
 
   test('BurnerWallet Tests', async ({ page }) => {
     await sessionTests(page, { connect, approveTransfer: async () => {} });
+    await connect(page);
+
+    const addressElement = await page.locator('#address');
+
+    let address: string | null = null;
+
+    if (addressElement) {
+      address = await addressElement.getAttribute('data-address');
+    }
+
+    if (address) {
+      await fundWallet({ publicKey: address });
+    } else {
+      throw new Error('Address is null');
+    }
+
+    await page.click('text=Disconnect');
+    await page.waitForSelector('text=/Connect Wallet/');
+
+    await transferTests(page, {
+      connect,
+      approveTransfer: async () => {},
+    });
   });
 });
