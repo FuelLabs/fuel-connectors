8cb94be68f16f9893651c5984e13eedacc808e16
Nelito Junior <ngvjqp@gmail.com>
2024-11-15 16:04:17 -0300

    feat(tests): integrate fundWallet function in WalletConnectConnector tests

--- e2e-tests/runner/examples/connectors/WalletConnectConnector/WalletConnectConnector.test.ts
+++ e2e-tests/runner/examples/connectors/WalletConnectConnector/WalletConnectConnector.test.ts
@@ -1,13 +1,10 @@
-import {
-  downloadFuel,
-  getButtonByText,
-  getByAriaLabel,
-} from '@fuels/playwright-utils';
+import { getButtonByText, getByAriaLabel } from '@fuels/playwright-utils';
 import { testWithSynpress } from '@synthetixio/synpress';
 import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright';
-import { sessionTests, skipBridgeFunds } from '../../../common/common';
+import { sessionTests, transferTests } from '../../../common/common';
 import type { ConnectorFunctions } from '../../../common/types';
 import basicSetup from '../../../wallet-setup/basic.setup';
+import { fundWallet } from '../setup';
 
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 
@@ -46,5 +43,26 @@ test.describe('WalletConnectConnector', () => {
 
   test('Fuel tests', async ({ page }) => {
     await sessionTests(page, { connect, approveTransfer });
+
+    await connect(page);
+
+    const addressElement = await page.locator('#address');
+    let address: string | null = null;
+    if (addressElement) {
+      address = await addressElement.getAttribute('data-address');
+    }
+
+    if (address) {
+      await fundWallet({ publicKey: address });
+    } else {
+      throw new Error('Address is null');
+    }
+
+    await page.click('text=Disconnect');
+    await page.waitForSelector('text=/Connect Wallet/');
+
+    await transferTests(page, { connect, approveTransfer });
+
+    // await incrementTests(page, { connect, approveTransfer });
   });
 });
