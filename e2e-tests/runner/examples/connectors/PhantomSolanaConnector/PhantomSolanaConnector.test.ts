d0f1ac88a8a3a9d70d631fccc9d03e1a4aa763d4
Nelito Junior <ngvjqp@gmail.com>
2024-11-21 16:30:48 -0300

    test(e2e): refactor PhantomSolanaConnector tests to integrate Phantom commands and improve connection flow

--- e2e-tests/runner/examples/connectors/PhantomSolanaConnector/PhantomSolanaConnector.test.ts
+++ e2e-tests/runner/examples/connectors/PhantomSolanaConnector/PhantomSolanaConnector.test.ts
@@ -1,53 +1,52 @@
 import { getButtonByText, getByAriaLabel } from '@fuels/playwright-utils';
+import type { Page } from '@playwright/test';
 import { testWithSynpress } from '@synthetixio/synpress';
-import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright';
 import { sessionTests, transferTests } from '../../../common/common';
-import type { ConnectorFunctions } from '../../../common/types';
+// import * as phantom from "@phantom/synpress/commands/phantom";
+import phantom from '../../../node_modules/@phantom/synpress/commands/phantom';
 import basicSetup from '../../../wallet-setup/basic.setup';
 import { fundWallet } from '../setup';
+import { test } from './setup';
 
-const test = testWithSynpress(metaMaskFixtures(basicSetup));
+phantom.confirmTransaction = async () => {
+  const notificationPage =
+    await phantom.playwright.switchToNotification('phantom');
+  await phantom.playwright.waitAndClick(
+    'phantom',
+    phantom.transactionPageElements.buttons.confirmTransaction, // Ensure this locator exists or define it
+    notificationPage,
+    { waitForEvent: 'close' },
+  );
+  return true;
+};
 
 test.describe('PhantomSolanaConnector', () => {
-  let metamask: MetaMask;
+  test.slow();
 
-  test.beforeEach(async ({ context, extensionId, metamaskPage, page }) => {
-    metamask = new MetaMask(
-      context,
-      metamaskPage,
-      basicSetup.walletPassword,
-      extensionId,
-    );
-    await page.goto('/', { waitUntil: 'domcontentloaded' });
-  });
-
-  const connect: ConnectorFunctions['connect'] = async (page) => {
+  const connect = async (page: Page) => {
     await page.goto('/');
     const connectButton = getButtonByText(page, 'Connect Wallet', true);
     await connectButton.click();
     await getByAriaLabel(page, 'Connect to Solana Wallets', true).click();
 
     await page.getByText('Proceed anyway').click();
-    await getButtonByText(page, 'MetaMask', true).click();
+    await getButtonByText(page, 'Phantom').click();
 
-    await metamask.connectToDapp();
-    // wait 3 seconds for the connection to be established
+    await phantom.acceptAccess();
     await page.waitForTimeout(3000);
-
-    await metamask.confirmSignature();
   };
 
-  const approveTransfer: ConnectorFunctions['approveTransfer'] = async () => {
-    await metamask.confirmTransaction();
+  const approveTransfer = async () => {
+    await phantom.confirmTransaction();
   };
 
   test('Fuel tests', async ({ page }) => {
-    await sessionTests(page, { connect, approveTransfer });
+    // await sessionTests(page, { connect, approveTransfer });
 
     await connect(page);
 
     const addressElement = await page.locator('#address');
-    let address: string | null = null;
+    let address = null;
     if (addressElement) {
       address = await addressElement.getAttribute('data-address');
     }
@@ -58,10 +57,7 @@ test.describe('PhantomSolanaConnector', () => {
       throw new Error('Address is null');
     }
 
-    await page.click('text=Disconnect');
-    await page.waitForSelector('text=/Connect Wallet/');
-
-    await transferTests(page, { connect, approveTransfer });
+    await transferTests(page, { connect, approveTransfer, keepSession: true });
 
     // await incrementTests(page, { connect, approveTransfer });
   });
