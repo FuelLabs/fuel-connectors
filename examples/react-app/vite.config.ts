d5f0f95a0040dbae212ec3b44f1b7bdcee46591c
Nelito Junior <ngvjqp@gmail.com>
2024-11-22 19:03:04 -0300

    feat: add node polyfills plugin to Vite configuration

--- examples/react-app/vite.config.ts
+++ examples/react-app/vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer', 'crypto', 'stream', 'util'],
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
