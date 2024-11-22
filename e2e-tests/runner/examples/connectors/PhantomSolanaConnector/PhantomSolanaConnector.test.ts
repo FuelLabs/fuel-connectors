4c2b09cc0716e272dbedb0d2cb1d1f76db8f1346
Nelito Junior <ngvjqp@gmail.com>
2024-11-22 10:33:32 -0300

    test(e2e): clean up PhantomSolanaConnector tests by removing commented-out code and unused functions

--- e2e-tests/runner/examples/connectors/PhantomSolanaConnector/PhantomSolanaConnector.test.ts
+++ e2e-tests/runner/examples/connectors/PhantomSolanaConnector/PhantomSolanaConnector.test.ts
@@ -4,12 +4,7 @@ import {
   getByAriaLabel,
 } from '@fuels/playwright-utils';
 import type { Page } from '@playwright/test';
-import { testWithSynpress } from '@synthetixio/synpress';
-import { sessionTests, transferTests } from '../../../common/common';
-// import * as phantom from "@phantom/synpress/commands/phantom";
 import phantom from '../../../node_modules/@phantom/synpress/commands/phantom';
-import basicSetup from '../../../wallet-setup/basic.setup';
-import { fundWallet } from '../setup';
 import { test } from './setup';
 
 phantom.confirmTransaction = async () => {
@@ -32,40 +27,21 @@ test.describe('PhantomSolanaConnector', () => {
     const connectButton = getButtonByText(page, 'Connect Wallet', true);
     await connectButton.click();
     await getByAriaLabel(page, 'Connect to Solana Wallets', true).click();
-
     await page.getByText('Proceed anyway').click();
     await getButtonByText(page, 'Phantom').click();
-
     await phantom.acceptAccess();
     await page.waitForTimeout(3000);
   };
 
-  // const approveTransfer = async () => {
-  //   await phantom.confirmTransaction();
-  // };
-
   test('Fuel tests', async ({ page }) => {
-    // await sessionTests(page, { connect, approveTransfer });
-
     await connect(page);
     const addressElement = await page.locator('#address');
     let address = null;
     if (addressElement) {
       address = await addressElement.getAttribute('data-address');
     }
-
     test.step('Check if address is not null', () => {
       expect(address).not.toBeNull();
     });
-
-    // if (address) {
-    //   await fundWallet({ publicKey: address });
-    // } else {
-    //   throw new Error('Address is null');
-    // }
-
-    // await transferTests(page, { connect, approveTransfer, keepSession: true });
-
-    // await incrementTests(page, { connect, approveTransfer });
   });
 });
