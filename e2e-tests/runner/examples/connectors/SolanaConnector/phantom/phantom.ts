aa877856d4dea9d87f78919968236c72d9428f2a
Nelito Junior <ngvjqp@gmail.com>
2024-11-28 22:11:06 -0300

    refactor(e2e): streamline SolanaConnector by replacing phantom commands with phantomCommands for better maintainability

--- e2e-tests/runner/examples/connectors/SolanaConnector/phantom/phantom.ts
+++ e2e-tests/runner/examples/connectors/SolanaConnector/phantom/phantom.ts
@@ -1,18 +1,7 @@
-import phantom from '../../../../node_modules/@phantom/synpress/commands/phantom.js';
+import phantomCommands from '../../../../node_modules/@phantom/synpress/commands/phantom';
 
 export const phantomExtended = {
-  ...phantom,
-  confirmTransaction: async () => {
-    const notificationPage =
-      await phantom.playwright.switchToNotification('phantom');
-    await phantom.playwright.waitAndClick(
-      'phantom',
-      phantom.transactionPageElements.buttons.confirmTransaction,
-      notificationPage,
-      { waitForEvent: 'close' },
-    );
-    return true;
-  },
+  ...phantomCommands,
 };
 
 export default phantomExtended;
