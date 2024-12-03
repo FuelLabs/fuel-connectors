580a8b113745525b611fdb8849390485b26351b6
Nelito Junior <ngvjqp@gmail.com>
2024-12-02 22:33:24 -0300

    tests: new .env setup

--- packages/evm-predicates/fuels.config.ts
+++ packages/evm-predicates/fuels.config.ts
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv';
 import { createConfig } from 'fuels';
 
+dotenv.config({
+  path: ['.env.local', '.env'],
+});
+
 export default createConfig({
-  providerUrl:
-    process.env.PROVIDER_URL || 'https://testnet.fuel.network/v1/graphql',
+  providerUrl: 'http://localhost:4000/v1/graphql',
   predicates: ['./predicate'],
   output: './src/generated/tmp',
   forcBuildFlags: ['--release'],
   // needs the private key to deploy the predicate
-  privateKey: process.env.PRIVATE_KEY || '0x',
+  privateKey:
+    '0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298',
 });
