fe0aed8c68c4689d26497eeaad8386f0ee081786
Anderson Arboleya <anderson@arboleya.me>
2025-04-14 09:48:05 -0300

    Fixing scripts for Predicate versioning; adding `txIdEncodders` to the mix
    Now versioning scripts works for both EVM and SOL predicates, and for EVM,
    it will also update the `txIdEncoders` file with any missing predicate.
    
    It's up to the engineer to review and commit these changes.

--- packages/solana-connector/scripts/versions.ts
+++ packages/solana-connector/scripts/versions.ts
@@ -5,4 +5,4 @@ import { generateVersions } from '@fuel-connectors/common/scripts';
 const __filename = fileURLToPath(import.meta.url);
 const currentDir = path.dirname(__filename);
 const __dirname = path.normalize(`${currentDir}/../src/generated`);
-await generateVersions(__dirname);
+await generateVersions(__dirname, 'Sol');
