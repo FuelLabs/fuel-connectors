2188f6ef6d2eb6a541544720e5b4eabcbd6a8a30
Anderson Arboleya <anderson@arboleya.me>
2025-04-15 19:05:38 -0300

    Updating `TxIdencoders` file during EVM predicate re-generation

--- packages/evm-predicates/scripts/versions.ts
+++ packages/evm-predicates/scripts/versions.ts
@@ -12,3 +12,39 @@ const __dirname = path.normalize(`${currentDir}/../src/generated`);
  * Generate the versions of the predicates.
  */
 const versions = await generateVersions(__dirname, 'Evm');
+
+/**
+ * Sync the txIdEncoders.
+ */
+console.log('Syncing txIdEncoders...');
+
+const encoderIds = Object.keys(txIdEncoders);
+const predicateIds = versions.map((v) => v.version);
+const missingIds = predicateIds.filter((id) => !encoderIds.includes(id));
+
+const rootDir = join(__dirname, '..', '..', '..');
+const txIdEncodersPath = join(
+  rootDir,
+  'evm-predicates',
+  'src',
+  'utils',
+  'txIdEncoders.ts',
+);
+
+let newTxIdEncoders = '';
+
+for (const id of missingIds) {
+  const encoder = `\n  '${id}': {\n    encodeTxId: encodeTxIdUtf8,\n  },`;
+  newTxIdEncoders += encoder;
+}
+
+const contents = readFileSync(txIdEncodersPath, 'utf8');
+
+const updatedContents = contents.replace(
+  /TxIdEncoder> = {/,
+  `TxIdEncoder> = {${newTxIdEncoders}`,
+);
+
+writeFileSync(txIdEncodersPath, updatedContents);
+
+console.log('TxIdEncoders synced.', txIdEncodersPath);
