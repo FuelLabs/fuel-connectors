ea84c9209d23c82c1c4b1583c9fb0519e2b70286
Anderson Arboleya <anderson@arboleya.me>
2025-04-29 17:31:01 -0300

    Accounting for possible `undefined` value

--- packages/walletconnect-connector/src/WalletConnectConnector.ts
+++ packages/walletconnect-connector/src/WalletConnectConnector.ts
@@ -457,6 +457,13 @@ export class WalletConnectConnector extends PredicateConnector {
     }
 
     const encoder = txIdEncoders[this.predicateAddress];
+
+    if (!encoder) {
+      throw new Error(
+        `TxIdEncoder not found for this predicate address: ${this.predicateAddress}`,
+      );
+    }
+
     return encoder.encodeTxId(txId);
   }
 
