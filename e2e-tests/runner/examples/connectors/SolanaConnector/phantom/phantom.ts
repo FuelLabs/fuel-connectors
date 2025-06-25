a95ef064d747cf4d17fca68f6f279a9c6b546e84
Peter Smith <peter@blueoceancomputing.co.uk>
2025-06-25 08:29:12 +0100

    Fixed the Solana connector tests

--- e2e-tests/runner/examples/connectors/SolanaConnector/phantom/phantom.ts
+++
@@ -1,7 +0,0 @@
-import phantomCommands from '../../../../node_modules/@phantom/synpress/commands/phantom';
-
-export const phantomExtended = {
-  ...phantomCommands,
-};
-
-export default phantomExtended;
